cbf95ccd1b603255cd8f8fd2da7ed57a1b49b883
shahinkhalajestani <vatharishok@gmail.com>
2024-08-12 22:40:38 +0330

    initials.

---
+++ src/test/java/com/shahinkhalajestani/banktask/BankTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shahinkhalajestani.banktask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
