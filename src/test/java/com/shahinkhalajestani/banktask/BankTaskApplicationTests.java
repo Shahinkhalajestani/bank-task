24d6860126f79bec0a0b63d406abbaca3540abdd
sh.khalajestani <sh.khalajestani@mydigipay.com>
2024-08-13 16:43:34 +0330

    wrote unit tests.

--- src/test/java/com/shahinkhalajestani/banktask/BankTaskApplicationTests.java
+++ src/test/java/com/shahinkhalajestani/banktask/BankTaskApplicationTests.java
@@ -2,12 +2,14 @@ package com.shahinkhalajestani.banktask;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("classpath:application-test.properties")
 class BankTaskApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
